ece09757d0d7afc20aa68234f751836bdfea65f1
thembones79 <michal.szumnarski@gmail.con>
2023-11-08 00:47:40 +0100

    finished

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -22,22 +23,24 @@ func main() {
 
 	}
 
-	for range links {
-		fmt.Println(<-c)
+	for l := range c {
+        go func(link string) {
+			time.Sleep(2 * time.Second)
+			checkLink(link, c)
+		}(l)
 
 	}
 
 }
 
 func checkLink(link string, c chan string) {
-
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep, it is up"
+	c <- link
 }
